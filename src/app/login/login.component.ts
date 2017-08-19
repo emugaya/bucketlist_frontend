bc637506d881595dc284d48612ef0ada1160b8b3
Ezekiel Mugaya <emugaya@gmail.com>
2017-08-19 21:12:33 +0300

    Refactored login component to display error mesagge when username and password are incorrect

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {NgForm} from '@angular/forms';
+import { NgForm} from '@angular/forms';
 import { AuthService} from '../services/auth.service';
-import {Http, Response } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { Router } from "@angular/router"
 
 import { Observable } from 'rxjs/Observable';
@@ -22,44 +22,33 @@ export class LoginComponent implements OnInit {
 	login_res:any ={};
 	data: Object
 
-	constructor(private authService: AuthService,private router: Router) { }
+	constructor( private authService: AuthService,
+				 private router: Router
+				 ) { }
 
   ngOnInit() {
   }
 
     onSubmit(f: NgForm): void {
-    	console.log(this.user);
     	this.user.username = f.value.username;
     	this.user.password = f.value.password;
-    	console.log("firsttoken");
-    	console.log(this.user);
-    	console.log("firsttoken");
-    	console.log(this.token);
     	this.authService.postLogin(this.user).subscribe((res: Response) => {
         	this.login_res = res.json();
-        	console.log(this.login_res);
         	this.token = this.login_res.token;
-        	console.log("secondtoken");
-        	console.log(this.token);
         	this.error_message = this.login_res.message;
-        	// console.log(this.error_message);
-        	// console.log(this.login_res);
+        	
         	if (this.login_res.error_message === "Invalid username or password"){
-        		console.log("else section executed");
+        		this.error_message = this.login_res.error_message;
         		this.router.navigate(['/login']);
         	
         	}
         	else {
-        		localStorage.setItem('currentuser', JSON.stringify({user: this.user, token: this.token}));
-        		console.log(localStorage.getItem('currentuser'));
-        		console.log("before routing");
+        		localStorage.setItem('currentuser', this.user.username);
+        		localStorage.setItem('token', this.token);
         		this.router.navigate(['/bucketlists']);
         	}
 
     	});
-
-    	console.log("new token is:" +this.token);
-    	// sessionStorage
 
 
 	}
