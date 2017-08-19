b0c542d2b6340c507b1f0093821fcc703c797a48
Ezekiel Mugaya <emugaya@gmail.com>
2017-08-19 21:13:31 +0300

    Setup of implementation of user registration.

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import { AuthService} from '../services/auth.service';
+import {Http, Response } from '@angular/http';
+import { Router } from "@angular/router"
+
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-register',
