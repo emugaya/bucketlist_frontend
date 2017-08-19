80ef9db0eac2c16ffd4e3ebbee9dd4227ce6d04f
Ezekiel Mugaya <emugaya@gmail.com>
2017-08-19 21:41:13 +0300

    Created methods to manage headers for post, delete, get, and put

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -11,16 +11,55 @@ import { SERVER } from '../server';
 export class AuthService {
 
   constructor(private http: Http) { 
-  	// console.log(this.postLogin)
   }
 
-  postLogin(user){
-  	let headers = new Headers;
-  	headers.append('Content-Type', 'application/json');
-    headers.append('Accept', 'application/json');
-    let requestoptions = new RequestOptions({
+// Headers to be used for post and  put
+  postHeaders(){
+  	  	let headers = new Headers;
+  	  	let token = sessionStorage.getItem('token')
+  		headers.append('Content-Type', 'application/json');
+    	headers.append('Accept', 'application/json');
+    	headers.append('Authorization', 'Basic '+btoa(token))
+    	let requestoptions = new RequestOptions({
             headers: headers
         });
-    return this.http.post(SERVER+'auth/login',user, requestoptions)
-		}
+        return requestoptions;
+
+  }
+  // Headers used for get and delete metthods
+
+  getHeaders(){
+  	  	let headers = new Headers;
+  	  	let token = sessionStorage.getItem('token')
+    	headers.append('Accept', 'application/json');
+    	headers.append('Authorization', 'Basic '+btoa(token))
+    	let requestoptions = new RequestOptions({
+            headers: headers
+        });
+
+        return requestoptions;
+  }
+  // Headers used for user registration and LongRunningScriptDetectedEvent
+
+  authHeaders(){
+  		let headers = new Headers;
+  		headers.append('Content-Type', 'application/json');
+    	headers.append('Accept', 'application/json');
+    	let requestoptions = new RequestOptions({
+            headers: headers
+        });
+        return requestoptions;
+  }
+
+  // This method handles user Login
+  postLogin(user){
+    	return this.http.post(SERVER+'auth/login',user, this.authHeaders());
+	}
+
+  // This method handles user Registration
+  postRegister(user) {
+  		return this.http.post(SERVER+'auth/register', user, this.authHeaders());
+
+  } 
+
 }
