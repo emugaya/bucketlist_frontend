3208274040e1b2b2db8ad222ef3b66bb88c71f30
Ezekiel Mugaya <emugaya@gmail.com>
2017-08-20 08:27:23 +0300

    Created stub methods for bucketlis  service

--- src/app/services/bucketlists.service.ts
+++ src/app/services/bucketlists.service.ts
@@ -1,8 +1,57 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+// Import Authentication service
+import { AuthService }from './auth.service';
+
+// Import server
+import { SERVER } from '../server';
 
 @Injectable()
 export class BucketlistsService {
 
-  constructor() { }
+  constructor(private authService: AuthService,
+  			  private http: Http) { }
+
+// Method to retrieve all buckets
+  getBuckets(){
+  	return this.http.get(SERVER+'bucketlists/',this.authService.getHeaders)
+
+  }
+
+// Method to retrieve single bucket with all items
+  getSingleBucket(){
+
+  }
+
+// Method to create buckets
+  postBucket(){
+
+  }
+
+// Method to edit bucket 
+  editBucket(){
+
+  }
+
+// Methid to delete single bucket
+  deleteBucket(){
+
+  }
+
+// Method to create items in bucketlist 
+  postItem(){
+
+  }
+
+// Method to edit bucket list items
+  editItem(){
+
+  }
+
+// Method to delete bucket list item
+  deleteItem(){
+
+  }
 
 }
